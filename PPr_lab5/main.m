304f0640a9f1939668c7c805e1407e4824d9b0ae
k2lhu <pgm550@gmail.com>
2014-12-09 14:50:04 +0200

    Initial Commit

---
+++ PPr_lab5/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PPr_lab5
+//
+//  Created by Roman on 12/9/14.
+//  Copyright (c) 2014 ininja. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
